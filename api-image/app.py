69b7a47acef7242f5bd3fcd2269d6a4d9e9f332a
Sebastiaan (Bas) Peters <sebastiaan.peters@orderbird.com>
2019-04-16 17:58:47 +0200

    initial commit

--- api-image/app.py
+++ api-image/app.py
@@ -10,6 +10,7 @@ option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
+
 app = Flask(__name__)
 CORS(app)
 
@@ -25,6 +26,8 @@ def vote():
     app.logger.info("received request")
 
     vote = None
+
+    # hello Epic!
 
     if request.method == 'POST':
         redis = get_redis()
