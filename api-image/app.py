ec036b638b0a451707d60258cc132d578aa0ed16
Jon Edvald <edvald@gmail.com>
2019-04-17 14:23:33 +0200

    Test commit

--- api-image/app.py
+++ api-image/app.py
@@ -10,15 +10,18 @@ option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
+
 app = Flask(__name__)
 CORS(app)
+
 
 def get_redis():
     if not hasattr(g, 'redis'):
         g.redis = Redis(host="redis-master", db=0, socket_timeout=5)
     return g.redis
 
-@app.route("/vote/", methods=['POST','GET'])
+
+@app.route("/vote/", methods=['POST', 'GET'])
 def vote():
     voter_id = hex(random.getrandbits(64))[2:-1]
 
@@ -34,16 +37,16 @@ def vote():
         redis.rpush('votes', data)
         print("Registered vote")
         response = app.response_class(
-          response=json.dumps(data),
-          status=200,
-          mimetype='application/json'
+            response=json.dumps(data),
+            status=200,
+            mimetype='application/json'
         )
         return response
 
     response = app.response_class(
-      response=json.dumps({}),
-      status=404,
-      mimetype='application/json'
+        response=json.dumps({}),
+        status=404,
+        mimetype='application/json'
     )
     return response
 
